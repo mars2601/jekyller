8d3d1c5278af888b088e911f7b1d55b0e0eccc4d
Marcel <pirnay.m@gmail.com>
2016-11-08 15:21:24 +0100

    ok
    a

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,13 @@
 
 requirejs.config({
     baseUrl: 'js/lib',
+    paths: {
+            jquery: 'jquery-3.1.1.min'
+        }
 });
 
-requirejs(['jquery-3.1.1.min', 'notify.min'], function ($, notify) {
+requirejs(['jquery', 'notify.min'], function ($, notify) {
     console.log("Let's work with our dependecies");
+
+    $(".fold-head").height(window.height());
 });
