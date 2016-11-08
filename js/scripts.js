e86f29af5e36c990104b560ff8dde8265778d52d
Marcel <pirnay.m@gmail.com>
2016-11-08 14:54:08 +0100

    ok

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,8 @@
+
 requirejs.config({
     baseUrl: 'js/lib',
-    paths: {
-        jquery: 'jquery-3.1.1.min'
-    }
 });
 
-requirejs(['jquery', 'notify.min'], function ($, notify) {
+requirejs(['jquery-3.1.1.min', 'notify.min'], function ($, notify) {
     console.log("Let's work with our dependecies");
-    console.log($("body"));
-    $.notify("Hello World");
 });
